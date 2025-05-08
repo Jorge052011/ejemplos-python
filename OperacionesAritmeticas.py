47b18292b2264583b31848c96ac4abfe4820534a
Jorge052011 <jimunozacuna+1@gmail.com>
2025-05-08 01:35:40 +0000

    W

---
+++ OperacionesAritmeticas.py
@@ -0,0 +1,36 @@
+# para hacer comentarios en una linea
+
+"""
+para hacer comentarios en varias lineas
+son con comillas
+"""
+
+
+#operaciones aritmeticas
+#suma
+print(3 + 3)
+#Resta
+print(3 - 3)
+#multiplicacion
+print(3 * 3)
+#division en que entrega el entero
+print(3 / 3) 
+#potencia
+print(3 ** 3)
+#modulo
+print(3 % 3)
+#división de flotantes agrega los flotantes o sea los decimales
+print(3 // 4)
+
+#tipos de datos
+print(type(10))
+print(type(3.14))
+print(type(1+3j))
+print(type("texto"))
+print(type([1,2,3,4,5,6,7]))
+print(type((9,3,4,5,6,7,8)))
+print(type((9,3,4,5,"5",5,6,)))
+print(type({"nombre":"juan"}))
+print(type(3<2))
+print(type({1,2,3,4,5,6,7,8}))
+print({1,2,3,4,5,6,7,8})
