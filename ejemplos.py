05ee0b3d5af29cfd5628154194ff076e09947f35
Jorge052011 <jimunozacuna+1@gmail.com>
2025-05-13 00:31:16 +0000

    W

---
+++ ejemplos.py
@@ -0,0 +1,51 @@
+## ejemplos de if -else
+
+## si es true 
+"""
+if condicion:
+    #se ejecuta
+    print("ejecucion.....")
+
+elif condicion:
+    print("imprime esto")
+elif condicion:
+    print("imprime esto")
+
+else:
+    print("termine") """   
+
+""" 
+edad = 21
+
+if edad > 18 :
+    #se ejecuta
+    print("entrada gratis")
+
+else:
+    print("no pasa")  """
+
+# iteradores
+
+lista = range (20)
+
+""" for index in lista:
+    print(f"iteración :{index}")
+
+print(lista)
+
+diccionario = {
+    'color': 'amarillo',
+    'color2': 'rojo'
+}
+ """
+""" for clave, valor in diccionario.items():
+    print(f"clave: {clave}, valor : {valor}")
+ """
+
+contador = 10
+
+while contador > 0:
+    contador = contador - 1  #forma normal pero no es la forma en que se lee habitualmente ya que se ocupa la forma siguinete abajo
+
+   # contador -= 1 es igual a contador = contador -1 #
+    print("imprie algo ")
